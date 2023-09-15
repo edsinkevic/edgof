3ac41f4847c10d41bb9d9d7fe3660138ffb3e2bc
Edvin Sinkevič <sinkevicedwin@gmail.com>
2023-09-15 17:38:04 +0300

    Initial commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+main.o:
+	gcc src/main.c -o build/main.o
+
+gof:
+	gcc build/main.o -o
---
+++ src/main.c
@@ -0,0 +1,12 @@
+//Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+//Any live cell with two or three live neighbours lives on to the next generation.
+//Any live cell with more than three live neighbours dies, as if by overpopulation.
+//Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+#include<stdio.h>
+
+
+int main() {
+  printf("Hello, world!\n");
+  return 0;
+}
