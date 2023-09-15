328022cb42be5d9bc64baf12933cbc8939cc3359
Edvin Sinkevič <sinkevicedwin@gmail.com>
2023-09-15 23:52:39 +0300

    Put the main function on top of the source file

--- src/main.c
+++ src/main.c
@@ -13,6 +13,66 @@
 #define WINDOW_WIDTH (GRID_WIDTH * GRID_CELL_SIZE) + 1
 #define WINDOW_HEIGHT (GRID_HEIGHT * GRID_CELL_SIZE) + 1
 
+char gof_handle_input();
+void gof_render_background(SDL_Renderer *renderer);
+void gof_render_grid(SDL_Renderer *renderer);
+void gof_render_lives(SDL_Renderer *renderer);
+
+
+int main(int argc, char *argv[]) {
+  SDL_Window *window = NULL;
+  SDL_Renderer *renderer = NULL;
+
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    fprintf(
+        stderr, 
+        "Could not initialize video: %s\n",
+        SDL_GetError());
+    goto main_error;
+  }
+
+  if (SDL_CreateWindowAndRenderer(
+      WINDOW_WIDTH,
+      WINDOW_HEIGHT,
+      0,
+      &window,
+      &renderer) < 0) {
+    fprintf(stderr,
+        "Could not create window and renderer: %s\n",
+        SDL_GetError());
+    goto main_error;
+  }
+
+  SDL_SetWindowTitle(window, "GAME OF LIFE");
+
+  while (gof_handle_input()) {
+    gof_render_background(renderer);
+    gof_render_grid(renderer);
+    gof_render_lives(renderer);
+    SDL_RenderPresent(renderer);
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return EXIT_SUCCESS;
+
+
+main_error:
+  if (renderer) {
+    SDL_DestroyRenderer(renderer);
+    renderer = NULL;
+  }
+
+  if (window) {
+    SDL_DestroyWindow(window);
+    window = NULL;
+  }
+
+  return EXIT_FAILURE;
+}
+
 char gof_handle_input() {
   SDL_bool should_continue = SDL_TRUE;
   SDL_Event event;
@@ -83,63 +143,4 @@ void gof_render_lives(SDL_Renderer *renderer) {
       }
     }
   }
-}
-
-
-
-int main(int argc, char *argv[]) {
-  SDL_Window *window = NULL;
-  SDL_Renderer *renderer = NULL;
-
-
-  SDL_Color grid_cursor_ghost_color = {44, 44, 44, 255};
-
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    fprintf(
-        stderr, 
-        "Could not initialize video: %s\n",
-        SDL_GetError());
-    goto main_error;
-  }
-
-  if (SDL_CreateWindowAndRenderer(
-      WINDOW_WIDTH,
-      WINDOW_HEIGHT,
-      0,
-      &window,
-      &renderer) < 0) {
-    fprintf(stderr,
-        "Could not create window and renderer: %s\n",
-        SDL_GetError());
-    goto main_error;
-  }
-
-  SDL_SetWindowTitle(window, "GAME OF LIFE");
-
-  while (gof_handle_input()) {
-    gof_render_background(renderer);
-    gof_render_grid(renderer);
-    gof_render_lives(renderer);
-    SDL_RenderPresent(renderer);
-  }
-
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-
-  return EXIT_SUCCESS;
-
-
-main_error:
-  if (renderer) {
-    SDL_DestroyRenderer(renderer);
-    renderer = NULL;
-  }
-
-  if (window) {
-    SDL_DestroyWindow(window);
-    window = NULL;
-  }
-
-  return EXIT_FAILURE;
 }
